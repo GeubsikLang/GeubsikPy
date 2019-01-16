af9c9e890eca725423988bfcea64b2e872f4105e
탁동혁 <tdh8316@naver.com>
2019-01-16 23:57:00 +0900

    initial commit

--- core/Except.py
+++ core/Except.py
@@ -1,3 +1,4 @@
 def syntax_error(code: str):
-    raise SyntaxError(f"님 코드 개이상하네요\n  =>{code}"
-                      "\n    ↑ 우리집 고양이가 해도 님보단 잘하겠어요")
+    raise SyntaxError(
+        f"님 코드 개이상하네요\n  =>{code}"
+        "\n    ↑ 우리집 고양이가 해도 님보단 잘하겠어요")
