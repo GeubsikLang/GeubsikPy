f40adadd96bb8da98466e36f0300af212ded73cb
탁동혁 <tdh8316@naver.com>
2019-01-18 00:14:40 +0900

    initial commit

--- core/Except.py
+++ core/Except.py
@@ -5,7 +5,8 @@ def syntax_error(*args, agent):
                 f"님 코드 개이상하네요;\n"
                 f"File '{agent.program_object.name}', line {agent.line + 1}\n"
                 f"  =>{agent.lexer.lines[agent.line]}\n"
-                "우리집 고양이가 해도 님보단 잘하겠어요!")
+                "\n우리집 고양이가 해도 님보단 잘하겠네요.\n"
+                "아 그런데 뭐가 문제인지는 모르겠어요 !")
 
     except Exception as err:
         print(*args)
